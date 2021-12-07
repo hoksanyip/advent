1871c22f944d1b58d010ccccca319bd9290cd928
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-07 07:49:58 +0100

    Add day 7

---
+++ src/main/python/day03.py
@@ -0,0 +1,27 @@
+import numpy as np
+from typing import Callable
+
+# Import
+with open("data/day03.txt", "r") as f:
+    data = np.array([int("0b" + row, 2) for row in f.readlines()])
+
+# Prepare
+k = int(np.ceil(np.log2(max(data))))
+def find_common(data: np.array, k: int, f: Callable) -> int:
+    data_iter = data.copy()
+    acc = 0
+    for i in range(k):
+        value = 2 ** (k - i - 1)
+        validation = data_iter >= value
+        which = f(np.mean(validation))
+        acc += value * which
+        selection = validation * which + (1 - validation) * (1 - which)
+        data_iter = data_iter[selection.astype(bool)] - which * value
+    return acc
+
+# Process
+oxygen = find_common(data, k, lambda x: x >= 0.5)
+scrubber = find_common(data, k, lambda x: (0 < x < 0.5) | (x == 1))
+
+# Output
+print(f"{oxygen * scrubber = }")
---
+++ src/main/scala/Day07.scala
@@ -0,0 +1,28 @@
+import cats._
+import cats.implicits._
+import cats.effect.{IO, IOApp}
+import cats.effect.unsafe.implicits.global
+import fs2.Stream
+
+object Day07 extends IOApp.Simple {
+  val sourceFile = "day07.txt"
+
+  def calculate_fuel(crabs: List[Int], pos: Int): Int =
+    def diff(x: Int): Int = math.abs(x - pos)
+    crabs.map(x => diff(x) * (diff(x) + 1) / 2).sum
+
+  def parseLine(line: String): List[Int] = line.split(",").map(_.toInt).toList
+  def filterLines(content: Stream[IO, List[Int]]): Stream[IO, List[Int]] = content
+  def processLines(stream: Stream[IO, List[Int]]): IO[Int] =
+    stream.compile.last.map(_.get).map { crabs =>
+      (0 to crabs.max).map(calculate_fuel(crabs, _)).min
+    }
+
+  def showOutput(result: Any): IO[Unit] = IO.println(s"Least fuel option: $result")
+
+  val lines = Parser.readContent(sourceFile)
+  val content = lines.through(Parser.parseLines(parseLine))
+  val filtered = filterLines(content)
+  val result = processLines(filtered)
+  val run = result >>= showOutput
+}
