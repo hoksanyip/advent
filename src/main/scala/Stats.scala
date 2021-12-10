46e945e90166ef435bebc34d074641248393ef95
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-10 22:48:53 +0100

    Reformat stats code

--- src/main/scala/Stats.scala
+++ src/main/scala/Stats.scala
@@ -9,21 +9,28 @@ import java.text.SimpleDateFormat
 import fs2.{text, Stream}
 import fs2.io.file.{Files, Path}
 
-
 object Stats extends IOApp.Simple {
   // Parser configurations
-  type Timestamp = Long
-  case class Star(get_star_ts: Timestamp)
+  case class Star(get_star_ts: Long)
   case class Member(
-      id: String, name: String, stars: Int,
-      global_score: Int, local_score: Int, last_star_ts: Timestamp,
-      completion_day_level: Map[Int, Map[Int, Star]])
+      id: String,
+      name: String,
+      stars: Int,
+      global_score: Int,
+      local_score: Int,
+      last_star_ts: Long,
+      completion_day_level: Map[Int, Map[Int, Star]]
+  )
   case class Board(owner_id: Int = 0, event: Int = 0, members: Map[Int, Member] = Map.empty)
   case class Stat(
-      name: String = "", stars: Int = 0, local_score: Int = 0,
-      day: Int = 0, part: Int = 0, timestamp: String = ""
+      name: String = "",
+      stars: Int = 0,
+      local_score: Int = 0,
+      day: Int = 0,
+      part: Int = 0,
+      timestamp: String = ""
   ) {
-    def toCsv = s"$day, $part, $timestamp, $name, $stars, $local_score" 
+    def toCsv = s"$day, $part, $timestamp, $name, $stars, $local_score"
   }
   def parseTimestamp(epoch: Long) = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(epoch * 1000L)
 
@@ -35,23 +42,22 @@ object Stats extends IOApp.Simple {
   val lines: Stream[IO, String] = Parser.readContent(sourceFile)
   val content: IO[String] = lines.compile.toList.map(_.mkString("\n"))
   val stats = content.map { json =>
-    decode[Board](json).getOrElse(Board())
+    decode[Board](json)
+      .getOrElse(Board())
       .members
       .map(_._2)
-      .toSeq
-      .sortWith(_.local_score > _.local_score)                                      // Order score
-      .map(m => (Stat(m.name, m.stars, m.local_score), m.completion_day_level))     // Fill stats
-      .map((stat, day) => (stat, day.toSeq.sortBy(_._1)))                           // Sort by day
-      .flatMap((stat, day) => day.map((k,v) => (stat.copy(day=k), v)))              // Flatten day
-      .map((stat, part) => (stat, part.toSeq.sortBy(_._1)))                         // Sort by part
-      .flatMap((stat, part) => part.map((k,v) => (stat.copy(part=k), v)))           // Flatten part
+      .map(m => (Stat(m.name, m.stars, m.local_score), m.completion_day_level)) // Fill stats
+      .flatMap((stat, day) => day.map((k, v) => (stat.copy(day = k), v))) // Flatten day
+      .flatMap((stat, part) => part.map((k, v) => (stat.copy(part = k), v))) // Flatten part
       .map((stat, part) => stat.copy(timestamp = parseTimestamp(part.get_star_ts))) // Convert time
+      .toSeq // Sort values
+      .sortBy(_.timestamp)
       .sortBy(stat => -stat.day * 10 + -stat.part)
   }
 
   // Write to output
   val headers = Stream.emit[IO, String]("day, part, timestamp, name, stars, score")
-  val values = Stream.eval(stats).flatMap(Stream.apply(_:_*)).map(_.toCsv)
+  val values = Stream.eval(stats).flatMap(Stream.apply(_: _*)).map(_.toCsv)
   val run = (headers ++ values)
     .intersperse("\n")
     .through(text.utf8.encode)
