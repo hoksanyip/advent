0d49c57addb9c1f2a2731966008ca99f743fdb9d
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-10 13:09:40 +0100

    Clean up day 10 scala

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -8,48 +8,48 @@ import fs2.Stream
 object Day10 extends IOApp.Simple {
   val sourceFile = "day10.txt"
 
-  enum ChunkType(val score: Long):
-    case `()` extends ChunkType(1L)
-    case `[]` extends ChunkType(2L)
-    case `{}` extends ChunkType(3L)
-    case `<>` extends ChunkType(4L)
-  type Inception = List[ChunkType]
-  type ChunkWrapper[A] = Either[ChunkType, A]
-  import ChunkType._
+  enum Chunk(val score: Long):
+    case Parenthese extends Chunk(1L)
+    case Bracket extends Chunk(2L)
+    case Brace extends Chunk(3L)
+    case Chevron extends Chunk(4L)
 
-  object SyntaxParser {
-    def open(chunkType: ChunkType) = Kleisli { (stack: Inception) =>
-      (chunkType :: stack).asRight[ChunkType]
+  type Inception = List[Chunk]
+  type Check[A] = Either[Chunk, A]
+
+  object Syntax {
+    def open(chunk: Chunk) = Kleisli { (stack: Inception) =>
+      (chunk :: stack).asRight[Chunk]
     }
-    def close(chunkType: ChunkType) = Kleisli { (stack: Inception) =>
+    def close(chunk: Chunk) = Kleisli { (stack: Inception) =>
       stack match {
-        case `chunkType` :: tail => Right(tail)
-        case _                   => Left(chunkType)
+        case `chunk` :: tail => Right(tail)
+        case _               => Left(chunk)
       }
     }
     def evalOne(symbol: Char) =
       symbol match {
-        case '(' => open(`()`)
-        case ')' => close(`()`)
-        case '[' => open(`[]`)
-        case ']' => close(`[]`)
-        case '{' => open(`{}`)
-        case '}' => close(`{}`)
-        case '<' => open(`<>`)
-        case '>' => close(`<>`)
+        case '(' => open(Chunk.Parenthese)
+        case ')' => close(Chunk.Parenthese)
+        case '[' => open(Chunk.Bracket)
+        case ']' => close(Chunk.Bracket)
+        case '{' => open(Chunk.Brace)
+        case '}' => close(Chunk.Brace)
+        case '<' => open(Chunk.Chevron)
+        case '>' => close(Chunk.Chevron)
       }
-    def eval(input: String): ChunkWrapper[Inception] =
+    def eval(input: String): Check[Inception] =
       input.toList.map(evalOne).reduce(_ andThen _).run(List.empty)
   }
 
-  def parseLine(line: String): ChunkWrapper[Inception] = SyntaxParser.eval(line)
-  def filterLines(content: Stream[IO, ChunkWrapper[Inception]]): Stream[IO, Inception] =
+  def parseLine(line: String): Check[Inception] = Syntax.eval(line)
+  def filterLines(content: Stream[IO, Check[Inception]]): Stream[IO, Inception] =
     content
       .map(_.fold(_ => List.empty, r => r))
       .filter(_.size > 0)
   def processLines(stream: Stream[IO, Inception]): IO[Long] =
-    stream.compile.toList.map(chunk =>
-      val scores = chunk.map(_.map(_.score).foldLeft(0L)(_ * 5 + _)).sorted
+    stream.compile.toList.map(inceptions =>
+      val scores = inceptions.map(_.map(_.score).foldLeft(0L)(_ * 5 + _)).sorted
       scores((scores.size / 2).toInt)
     )
   def showOutput(result: Long): IO[Unit] =
