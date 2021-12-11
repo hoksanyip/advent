ea1b378823fdf5bd81572020e1247a346091cdd5
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-11 21:05:43 +0100

    Add day 11 in python

---
+++ src/main/python/day11.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# Import
+with open("src/main/resources/day11.txt", "r") as f:
+    data = np.array([list(row.strip()) for row in f.readlines()]).astype(int)
+
+# Prepare
+n, _ = data.shape
+area = np.eye(n, dtype=int)
+area[1:] += np.eye(n, dtype=int)[:-1]
+area[:, 1:] += np.eye(n, dtype=int)[:, :-1]
+
+flashes = 0
+t = 0
+while not (data == 0).all():
+    # Perform step
+    t += 1
+    data += 1
+    # Check flashes
+    has_flash = new_flash = data > 9
+    while new_flash.any():
+        # Spread flash to neighbours  
+        data += area @ new_flash.astype(int) @ area
+        # Check for new flashes
+        new_flash = (data > 9) & (~has_flash)
+        # Update existing flashes
+        has_flash = (data > 9)
+    # Set flashed ones (if any) to zero
+    data[has_flash] = 0
+    # Count number of flashes so far
+    flashes += has_flash.sum()
+
+# Output
+print(f"{flashes = }")
+print(f"{t = }")
