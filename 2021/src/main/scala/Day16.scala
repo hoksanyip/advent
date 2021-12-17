9480d479e44a30c10e64a75cb9711e2b590c9b9d
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-17 02:04:56 +0100

    Improve day 16 scala

--- 2021/src/main/scala/Day16.scala
+++ 2021/src/main/scala/Day16.scala
@@ -21,8 +21,8 @@ import scala.util.chaining._
     * **********************************************
     */
   trait Content
-  case class Package(version: Long, typeId: Long, id: Long, data: List[Content]) extends Content
-  case class Data(value: Long) extends Content
+  case class Operator(version: Long, typeId: Long, data: List[Content]) extends Content
+  case class Operand(version: Long, typeId: Long, data: Long) extends Content
 
   object Parser {
 
@@ -32,30 +32,26 @@ import scala.util.chaining._
     def take(n: Int) = State[String, String] { s => if (s.size < n) "" -> s else s.splitAt(n).swap }
 
     // Parse single package
-    def parse: Parser[Package] =
+    def parse: Parser[Content] =
       for {
         // Retrieve header info
         version <- take(3).map(toLong)
         typeId  <- take(3).map(toLong)
 
         // Define length of subpackages if applicable
-        id <-
-          if (typeId == 4) take(0).map(_ => -1L)
-          else take(1).map(toLong)
-        n <-
-          if (id == 0) take(15).map(toLong).map(_.toInt)
-          else if (id == 1) take(11).map(toLong).map(_.toInt)
-          else take(0).map(_ => -1)
-
+        id      <- if (typeId == 4)  take(0).map(_ => -1L)
+                   else              take(1).map(toLong)
+        n       <- if (id == 0)      take(15).map(toLong).map(_.toInt)
+                   else if (id == 1) take(11).map(toLong).map(_.toInt)
+                   else              take(0).map(_ => -1)
         // Get content
-        len <- peekSize
-        content <-
-          if (id == 0) parseSize(len - n)
-          else if (id == 1) parseN(n)
-          else parseData.map(b => List(Data(toLong(b))))
-      } yield Package(version, typeId, id, content)
+        len     <- peekSize
+        pkg     <- if (id == 0)      parseSize(len - n).map(d => Operator(version, typeId, d))
+                   else if (id == 1) parseN(n).map(d => Operator(version, typeId, d))
+                   else              parseData.map(b => Operand(version, typeId, toLong(b)))
+      } yield pkg
 
-    // Parse literal data
+    // Parse literal in binary format
     def parseData: Parser[String] = for {
       head <- take(5)
       tail <- if (head.head == '1') parseData else take(0).map(_ => "")
@@ -71,9 +67,8 @@ import scala.util.chaining._
     def parseSize(n: Int): Parser[List[Content]] = for {
       head <- parse
       len  <- peekSize
-      tail <-
-        if (len > n) parseSize(n)
-        else take(0).map(_ => Nil)
+      tail <- if (len > n) parseSize(n)
+              else         take(0).map(_ => Nil)
     } yield (head :: tail)
   }
 
@@ -81,11 +76,10 @@ import scala.util.chaining._
     * Process
     * **********************************************
     */
-  object Processor {
-    def apply(pkg: Content): Long = pkg match
-      case Data(value) => value.toLong
-      case Package(version, typeId, id, data) =>
-        val values = data.map(Processor.apply)
+  def process(pkg: Content): Long = pkg match
+      case Operand(_, _, value) => value.toLong
+      case Operator(_, typeId, data) =>
+        val values = data.map(process)
         typeId match
           case 0 => values.reduce(_ + _)
           case 1 => values.reduce(_ * _)
@@ -95,9 +89,9 @@ import scala.util.chaining._
           case 5 => values.pipe { l => if (l(0) > l(1)) 1 else 0 }
           case 6 => values.pipe { l => if (l(0) < l(1)) 1 else 0 }
           case 7 => values.pipe { l => if (l(0) == l(1)) 1 else 0 }
-  }
+
   val content = Parser.parse.runA(binaries).value
-  val result = Processor(content)
+  val result = process(content)
 
   /** **********************************************
     * Output
