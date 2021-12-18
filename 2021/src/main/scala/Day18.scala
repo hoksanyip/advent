5d80e171a69e2f117ce9040ca43309dd794320bb
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-18 21:21:37 +0100

    Clean up day 18 scala

--- 2021/src/main/scala/Day18.scala
+++ 2021/src/main/scala/Day18.scala
@@ -16,28 +16,21 @@ import scala.annotation.tailrec
     * Prepare
     * **********************************************
     */
-  trait Tree { def magnitude: Int }
-  case class Leaf(value: Int) extends Tree {
-    override def toString = value.toString
-    def magnitude = value
-  }
-  case class Empty() extends Tree { def magnitude = 0 }
-  case class Branch(left: Tree = Empty(), right: Tree = Empty()) extends Tree {
-    override def toString = s"[${left.toString},${right.toString}]"
-    def magnitude = left.magnitude * 3 + right.magnitude * 2
-  }
+  sealed trait Tree
+  case class Leaf(value: Int) extends Tree
+  case class Branch(left: Tree, right: Tree) extends Tree
 
-  object Tree {
-    // Building blocks
+  object Parser {
     def drop = State[String, Unit] { source => (source.drop(1), ()) }
-    def parseLeaf = State[String, Tree] { _.toList match
+    def parseLeaf = State[String, Tree] {
+      _.toList match
         case head :: tail => (tail.mkString, Leaf(head.asDigit))
-        case _            => ("", Empty())
+        case _            => ("", Leaf(-1))
     }
-    def parseTree: State[String, Tree] = 
+    def parseTree: State[String, Tree] =
       for {
-        head    <- State.inspect[String, Char](_.head)
-        tree    <- if(head == '[') parseBranch else parseLeaf
+        head <- State.inspect[String, Char](_.head)
+        tree <- if (head == '[') parseBranch else parseLeaf
       } yield tree
     def parseBranch: State[String, Branch] =
       for {
@@ -45,37 +38,32 @@ import scala.annotation.tailrec
         right <- drop *> parseTree
         _     <- drop
       } yield Branch(left, right)
-
-    // Composition of building blocks
-    def apply(line: String) = parseTree.runA(line).value
+    def run(line: String): Tree = parseTree.runA(line).value
   }
-  val numbers = input.map(Tree.apply)
+  val numbers = input.map(Parser.run)
 
   /** **********************************************
     * Process
     * **********************************************
     */
-  object Operator {
-
+  object Tree {
     def explode(number: Tree): Either[Tree, Tree] =
       def accumulate(dl: Int, number: Tree, dr: Int): Tree =
         number match
           case Leaf(v)      => Leaf(dl + v + dr)
           case Branch(l, r) => Branch(accumulate(dl, l, 0), accumulate(0, r, dr))
-          case _            => number
-
       def _explode(number: Tree, level: Int = 0): Option[(Int, Tree, Int)] =
         number match
-          case Leaf(v)                                  => None
-          case Branch(Leaf(l), Leaf(r)) if (level >= 4) => Some(l, Leaf(0), r)
+          case Branch(Leaf(l), Leaf(r)) if (level >= 4) =>
+            Some(l, Leaf(0), r)
           case Branch(l, r) =>
             _explode(l, level + 1).map { (dl, v, dr) =>
               (dl, Branch(v, accumulate(dr, r, 0)), 0)
             } orElse _explode(r, level + 1).map { (dl, v, dr) =>
               (0, Branch(accumulate(0, l, dl), v), dr)
             }
-          case _ => None
-
+          case _ =>
+            None
       _explode(number).map(_._2).toLeft(number)
     end explode
 
@@ -86,17 +74,20 @@ import scala.annotation.tailrec
             Some(Branch(Leaf(r / 2), Leaf(r - r / 2)))
           case Branch(l, r) =>
             _split(l).map(v => Branch(v, r)).orElse { _split(r).map(v => Branch(l, v)) }
-          case _ => None
-
+          case _ =>
+            None
       _split(number).toLeft(number)
     end split
 
-    // Combinating
+    // Operators
     def reduce(number: Tree): Tree = explode(number).flatMap(l => split(l)).fold(reduce, identity)
-    def add(a: Tree, b: Tree): Tree = reduce(Branch(a, b))
+    def combine(a: Tree, b: Tree): Tree = reduce(Branch(a, b))
+    def magnitude(number: Tree): Int =
+      number match
+        case Leaf(v)      => v
+        case Branch(l, r) => magnitude(l) * 3 + magnitude(r) * 2
   }
-
-  val result = (numbers, numbers).tupled.map(Operator.add).map(_.magnitude).max
+  val result = (numbers, numbers).tupled.map(Tree.combine).map(Tree.magnitude).max
 
   /** **********************************************
     * Output
