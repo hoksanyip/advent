93bece5b183465d2582e820674c5901017435213
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-19 22:21:18 +0100

    Rewrite day 19

--- 2021/src/main/scala/Day19.scala
+++ 2021/src/main/scala/Day19.scala
@@ -3,243 +3,133 @@ import cats._
 import cats.implicits._
 import cats.data.State
 import scala.util.chaining._
+import scala.annotation.tailrec
+
 
 @main def Day19 = {
+
+  /** **********************************************
+    * Prepare
+    * **********************************************
+    */
+  case class Coord(c: (Int, Int, Int)) {
+    def x = c._1; def y = c._2; def z = c._3;   def l = List(x, y, z)
+    def `+` = move
+    def `-` = diff
+    override def toString = s"Coord($x, $y, $z)"
+    // Rotation object can have 1,2,3,-1,-2,-3 referring
+    // to the relative axis (x=1,y=2,z=3) combined with
+    // the mirroring component (sign of value)
+    def diff(s: Coord) = Coord(x - s.x, y - s.y, z - s.z)
+    def move(s: Coord) = Coord(x + s.x, y + s.y, z + s.z)
+    def rotate(r: Coord): Coord =
+      Coord(l(r.x.abs - 1) * r.x.sign, l(r.y.abs - 1) * r.y.sign, l(r.z.abs - 1) * r.z.sign)
+    def dist(a: Coord, level: Int = 2): Int = 
+      (l zip a.l).map(_ - _).map(d => math.pow(d.abs, level).toInt).sum
+  }
+  object Coord {
+    def apply(x: Int, y: Int, z: Int): Coord = new Coord((x, y, z))
+    def apply(line: String) = line match { case s"$x,$y,$z" =>
+      new Coord(x.toInt, y.toInt, z.toInt)
+    }
+  }
+  def zipMap[A, B](a: List[A])(f: (A, A) => B): List[B] =
+    a match
+      case head :: tail => tail.map(f(head, _)) ++ zipMap(tail)(f)
+      case _            => List.empty[B]
+
+  /** **********************************************
+    * Process
+    * **********************************************
+    */
+  object Scanner {
+
+    val rotations: List[Coord] = for {
+        x <- List(1, -1)
+        y <- List(1, -1)
+        z <- List(1, -1)
+        Seq(x2, y2, z2) <- (1 to 3).permutations.toList
+    } yield Coord(x2 * x, y2 * y, z2 * z)
+
+    def similarity(scannerA: List[Coord], scannerB: List[Coord]): Int =
+      def _pairDist(scanner: List[Coord]): Set[Int] =
+        zipMap(scanner) { (a, b) => (a dist b) }.toSet
+      (_pairDist(scannerA) & _pairDist(scannerB)).size
+
+    def align(scannerA: List[Coord], scannerB: List[Coord]): (Coord, Coord) = {
+      val diffs = for {
+        a <- scannerA
+        b <- scannerB
+        r <- rotations
+      } yield (r, a - b.rotate(r))
+      diffs.map(d => Map( d -> 1)).reduce(_ |+| _).toList.sortBy(-_._2).head._1
+    }
+  }
 
   /** **********************************************
     * Import data
     * **********************************************
     */
   val input = Source.fromFile("2021/src/main/resources/day19.txt").getLines.toList
-
-  /** **********************************************
-    * Prepare
-    * **********************************************
-    */
-
-  case class Coord(c: (Int, Int, Int)) extends AnyVal {
-    def l = List(x, y, z)
-    def x = c._1
-    def y = c._2
-    def z = c._3
-    def `-`(s: Coord) = Coord(x - s.x, y - s.y, z - s.z)
-    override def toString = s"Coord($x, $y, $z)"
-    // Rotation object can have 1,2,3,-1,-2,-3 referring
-    // to the relative axis (x=1,y=2,z=3) combined with
-    // the mirroring component (sign of value)
-    def rotate(rotation: Coord): Coord = 
-      Coord(
-        l(rotation.x.abs - 1) * rotation.x.sign,
-        l(rotation.y.abs - 1) * rotation.y.sign,
-        l(rotation.z.abs - 1) * rotation.z.sign,
-      )
-    def move(s: Coord): Coord = Coord(x + s.x, y + s.y, z + s.z)
-    def dist(a: Coord): Double = math.pow(a.x - x, 2) + math.pow(a.y - y, 2) + math.pow(a.z - z, 2)
-  }
-  object Coord {
-    def apply(x: Int, y: Int, z: Int): Coord = new Coord((x, y, z))
-    def apply(line: String) = line match
-      case s"$x,$y,$z" => new Coord(x.toInt, y.toInt, z.toInt)
-
-    val rotations = List(
-      Coord(1, 2, 3), Coord(1, -3, 2), Coord(1, 3, -2), Coord(1, -2, -3),     // x on +x
-      Coord(-2, 1, 3), Coord(-3, 1, -2), Coord(2, 1, -3), Coord(3, 1, 2),     // x on +y
-      Coord(2, 3, 1), Coord(-3, 2, 1), Coord(-2, -3, 1), Coord(3, -2, 1),     // x on +z
-      Coord(-1, -2, 3), Coord(-1, 3, 2), Coord(-1, -3, -2), Coord(-1, 2, -3), // x on -x
-      Coord(2, -1, 3), Coord(3, -1, -2), Coord(-2, -1, -3), Coord(-3, -1, 2), // x on -y
-      Coord(-2, 3, -1), Coord(3, 2, -1), Coord(2, -3, -1), Coord(-3, -2, -1)  // x on -z
-    )
-  }
-
-  /** **********************************************
-    * Process
-    * **********************************************
-    */
-  object Calibration {
-
-    def pairwiseDist(scanner: List[Coord]) =
-      scanner.zipWithIndex.flatMap { (a, iA) =>
-        scanner.zipWithIndex.filter( (b, iB) => iB > iA )
-          .map { (b, iB) => (iA,iB) -> (a dist b) }
-      }.toMap
-
-    def similarity(scannerA: List[Coord], scannerB: List[Coord]) =
-      val distA = pairwiseDist(scannerA).values.toSet
-      val distB = pairwiseDist(scannerB).values.toSet
-      (distA & distB).size
-
-    def merge(scannerA: List[Coord], scannerB: List[Coord]): (Coord, Coord) =
-      // Returns rotation and shift
-      val distA = pairwiseDist(scannerA)
-      val distB = pairwiseDist(scannerB)
-      val pairs = (distA.values.toSet & distB.values.toSet)
-      // Define distances per beacon Map[String, Map[String, Coord]] : idx => idx => Coord
-      val setA = distA
-        .filter(pairs contains _._2)
-        .pipe(s => s ++ s.map{ case ((a,b),c) => ((b,a),c) })
-        .groupMap(_._1._1){ case ((a,b), c) => (b,c)  }
-        .map((k,v) => k -> v.toList.sortBy(_._2))
-      val setB = distB
-        .filter(pairs contains _._2)
-        .pipe(s => s ++ s.map{ case ((a,b),c) => ((b,a),c) })
-        .groupMap(_._1._1){ case ((a,b), c) => (b,c)  }
-        .map((k,v) => k -> v.toList.sortBy(_._2))
-      // Find beacon matching with first beacon of first scanner
-      val firstAdist = setA.map((k,v) => k -> v.map(_._2).toSet).head
-      val firstBdist = setB.map((k,v) => k -> v.map(_._2).toSet).filter(_._2 == firstAdist._2).head
-      val firstA = firstAdist._1
-      val firstB = firstBdist._1
-      // Define shift to that beacon in second scanner
-      val coordA = scannerA(firstA)
-      val coordB = scannerB(firstB)
-      // Find coordinate changes to map with each other
-      val keysA = scannerA
-        .zipWithIndex.filter((v,i) => setA(firstA).map(_._1).toList contains i).map(_._1)
-        .map(_ - coordA).map { c => Coord(c.x * c.x, c.y * c.y, c.z * c.z)}
-        .reduce( (a,b) => Coord(a.x + b.x, a.y + b.y, a.z + b.z))
-      val keysB = scannerB
-        .zipWithIndex.filter((v,i) => setB(firstB).map(_._1).toList contains i).map(_._1)
-        .map(_ - coordB).map { c => Coord(c.x * c.x, c.y * c.y, c.z * c.z)}
-        .reduce( (a,b) => Coord(a.x + b.x, a.y + b.y, a.z + b.z))
-      // Get permutation
-      val perm = Coord(
-        keysB.l.zipWithIndex.filter(_._1 == keysA.x).head._2 + 1,
-        keysB.l.zipWithIndex.filter(_._1 == keysA.y).head._2 + 1,
-        keysB.l.zipWithIndex.filter(_._1 == keysA.z).head._2 + 1
-      )
-      // Get mirroring
-      val rotation = (
-        scannerA(setA(firstA).head._1) - coordA -
-        (scannerB(setB(firstB).head._1) - coordB).rotate(perm)
-      ).pipe { c =>
-        Coord(
-          perm.x * (if (c.x == 0) 1 else -1), 
-          perm.y * (if (c.y == 0) 1 else -1),
-          perm.z * (if (c.z == 0) 1 else -1)
-        )
-      }
-      // Get shift
-      val shift = coordA - coordB.rotate(rotation)
-      (rotation, shift)
-    end merge
-  }
-
-  def getLinkedList(doubleMap: Set[(Int, Int)])(todo: Set[Int] = Set(0), acc: Map[Int, Int]): Map[Int, Int] = {
-      if (todo.isEmpty) acc
-      else
-        val links = doubleMap
-          .filterNot(todo contains _._1)
-          .groupMap(_._2)(_._1).map((k,v) => k -> v.head)
-        getLinkedList(doubleMap)(todo -- links.keys, links ++ acc)
-  }
-
-  def matchScanner(id: Int, links: Map[Int, List[Int]], data: List[List[Coord]]): Map[Int, (Coord, Coord)] = {
-    if(!links.keys.toSet.contains(id)) Map.empty
-    else
-      links(id).map { i =>
-        val (rotation, shift) = merge(data(id), data(i))
-        Map(i -> (rotation, shift)) ++ 
-        matchScanner(i, links, data).map { case (i, (r, s)) => 
-          i -> (r.rotate(rotation), s.rotate(rotation).move(shift))
-        }
-      }.reduce(_ ++ _)
-  }
-
-  //***********************************
-  // ------------  Import data   ------
-  //***********************************
-  val input = Source.fromFile("2021/src/main/resources/day19.txt").getLines.toList
-  val data = {
-    input
-      .mkString("\n").split("\n\n")
-      .map(_.split("\n").drop(1).map(Coord.apply _).toList)
-      .toList
-  }
+  val data = input
+    .mkString("\n").split("\n\n")
+    .map(_.split("\n").drop(1).map(Coord.apply _).toList)
+    .toList
 
   //***********************************
   // Find overlapping scanners
   //***********************************
-  val matchings = data.zipWithIndex.flatMap { (scannerA, indexA) =>
-    data.zipWithIndex
-      .filter((s,i) => i > indexA)
-      .map { (scannerB, indexB) =>
-        Map((indexA, indexB) -> similarity(scannerA, scannerB))
-      }
-  }.reduce(_ |+|_).filter(_._2 >= 66)
+  @tailrec
+  def getDependency(simMat: Set[(Int, Int)])(todo: Set[Int] = Set(0), acc: Map[Int, Int]): Map[Int, Int] = {
+      if (todo.isEmpty) acc
+      else
+        val links = simMat
+          .filterNot(todo contains _._1)
+          .groupMap(_._2)(_._1).map((k,v) => k -> v.head)
+        getDependency(simMat)(todo -- links.keys, links ++ acc)
+  }
+
+  def mergeInto(id: Int, links: Map[Int, List[Int]], data: List[List[Coord]]): Map[Int, (Coord, Coord)] = {
+    if(!links.keys.toSet.contains(id)) Map.empty
+    else
+      links(id).map { i =>
+        val (rotation, shift) = Scanner.align(data(id), data(i))
+        val children = mergeInto(i, links, data).map { case (i, (r, s)) => 
+          i -> (r.rotate(rotation), s.rotate(rotation).move(shift))
+        }
+        Map(i -> (rotation, shift)) ++ children
+      }.reduce(_ ++ _)
+  }
+
+  // Calculate overlaps between scanners
+  val matchings = zipMap(data.zipWithIndex) { case ((a, iA), (b, iB)) =>
+    (iA, iB) -> Scanner.similarity(a, b)
+  }.toMap.filter(_._2 >= 66)
 
   // Get order of scanners to go through
-  val doubleMap = {
-    matchings.toList
-      .map( (k,v) => (k._1 -> k._2))
-      .flatMap( m => List(m, (m._2, m._1)))
-      .toSet
-  }
-  val todo = doubleMap.map(_._1) -- Set(0)
-  val links = getLinkedList(doubleMap)(todo, Map(0 -> 0)).toList.drop(1).groupMap(_._2)(_._1)
-  val calibrator = matchScanner(0, links, data) ++ Map(0 -> (Coord(1,2,3), Coord(0,0,0)))
-  val numBeacons = (0 to data.size - 1).flatMap { i =>
-    val (rotation, shift) = calibrator(i)
-      data(i).map(_.rotate(rotation).move(shift))
-  }.toSet.size
-  val scanners = calibrator.toList.map(_._2).map(_._2)
-
-  scanners.map { a =>
-    scanners.map { b =>
-      (a.x - b.x).abs + (a.y - b.y).abs + (a.z - b.z).abs
-    }.toList
-  }.flatten.max
-
-  val beacons = (0 to data.size - 1).map { i =>
-    val (rotation, shift) = calibrator(i)
-      data(i).map(_.rotate(rotation).move(shift))
-  }
-
-  beacons.zipWithIndex.flatMap { (be,i) =>
-    be.map { b =>
-      s"$i, ${b.x}, ${b.y}, ${b.z}"  
-    }
-  }.foreach(println)
-
-  beacons.flatten.toSet.foreach(b =>
-    println(s"${b.x}, ${b.y}, ${b.z}" )
-  )
-  scanners.foreach(b => println(s"${b.x}, ${b.y}, ${b.z}" ))
-
-  beacons.map(a =>
-    beacons.map(b =>
-      (a.toSet & b.toSet).size
-    ).mkString(",")
-  ).foreach(println)
-  for {
-    a <- beacons
-    b <- beacons
-  } yield (a.toSet & b.toSet).size
-
-  val distances = for {
-    a <- scanners.toList
-    b <- scanners.toList
-  } yield (a.x - b.x).abs + (a.y - b.y).abs + (a.z - b.z).abs
-
-  distances.max
-
-  scanners.map { a =>
-    scanners.map { b =>
-      (a.x - b.x).abs + (a.y - b.y).abs + (a.z - b.z).abs
-    }.toList
-  }.flatten.max
-
-
+  val fullMatch = matchings.toList.map(_._1).map(_.swap).flatMap( m => List(m, m.swap)).toSet
+  val todo = fullMatch.map(_._1) -- Set(0)
+  val links = getDependency(fullMatch)(todo, Map(0 -> 0)).toList.drop(1).groupMap(_._2)(_._1)
   
-  10618 -- 16685
+  // Find the projection based on scanner 0
+  val mapper = mergeInto(0, links, data) ++ Map(0 -> (Coord(1,2,3), Coord(0,0,0)))
 
+  // Find all locations
+  val beacons = (0 to data.size - 1).flatMap { i =>
+    mapper(i).pipe { (rotation, shift) =>
+      data(i).map(_.rotate(rotation).move(shift))
+    }
+  }.toSet
+  val scanners = mapper.toList.map(_._2).map(_._2)
 
-  def process = ???
+  // Calculate pairwise distances
+  val distances = zipMap(scanners.toList)((a,b) => a.dist(b, 1))
+
 
   /** **********************************************
     * Output
     * **********************************************
     */
-  def show(result: Any) = ???
-
-  println
+  println(s"Number of unique beacons: ${beacons.size}")
+  println(s"Largest distance between scanners: ${distances.max}")
 }
