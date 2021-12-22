4752a7be355386e0a08229492668491a9a240795
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-22 23:34:15 +0100

    remove OptionT from update

--- src/main/scala/Update.scala
+++ src/main/scala/Update.scala
@@ -1,4 +1,3 @@
-import cats.data.OptionT
 import cats.effect.{IO, IOApp}
 import cats.effect.unsafe.implicits.global
 import cats.implicits._
@@ -10,27 +9,25 @@ import scala.io.StdIn.readInt
 
 object Update extends IOApp.Simple {
 
-  val program = for
+  val run = for
     // Get config parameters
-    day       <- OptionT.liftF(IO { print("Please select day: "); readInt() })
-    sessionId <- OptionT.fromOption[IO](sys.env.get("SESSION_ID"))
-    year      <- OptionT.fromOption(sys.env.get("YEAR"))
-    uri       <- OptionT.some(uri"https://adventofcode.com/$year/day/$day/input")
+    _   <- IO.print("Please select day: ")
+    day <- IO.readLine.map(_.toInt)
+    sessionId = System.getenv("SESSION_ID")
+    year = System.getenv("YEAR")
+    uri = uri"https://adventofcode.com/$year/day/$day/input"
 
     // Send request
-    _       <- OptionT.liftF(IO { println(s"Requesting data from year $year and day $day") })
-    request <- OptionT.some(basicRequest.cookie("session" -> sessionId).get(uri))
-    content <- OptionT.fromOption(request.send(HttpURLConnectionBackend()).body.toOption)
+    _ <- IO.println(s"Requesting data from year $year and day $day")
+    request = basicRequest.cookie("session" -> sessionId).get(uri)
+    content = request.send(HttpURLConnectionBackend()).body.getOrElse("")
 
     // Write to output file
-    _ <- OptionT.liftF(IO { println(s"Write to output file") })
-    _ <- OptionT.liftF {
-      Stream(content)
-        .through(text.utf8.encode)
-        .through(Files[IO].writeAll(Path(s"$year/src/main/resources/day$day.txt")))
-        .compile
-        .drain
-    }
+    _ <- IO.println(s"Write to output file")
+    _ <- Stream(content)
+      .through(text.utf8.encode)
+      .through(Files[IO].writeAll(Path(s"$year/src/main/resources/day$day.txt")))
+      .compile
+      .drain
   yield ()
-  val run = program.value *> IO.unit
 }
