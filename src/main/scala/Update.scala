c41e1d17b923c64db666e9679219380c15cf9aaf
Hoksan Yip <hoksanyip@Hoksans-MacBook-Pro.local>
2021-12-23 07:30:11 +0100

    Dont ask for day

--- src/main/scala/Update.scala
+++ src/main/scala/Update.scala
@@ -1,7 +1,6 @@
 import cats.effect.{IO, IOApp}
 import cats.effect.unsafe.implicits.global
 import cats.implicits._
-import java.text.SimpleDateFormat
 import fs2.io.file.{Files, Path}
 import fs2.{text, Stream}
 import sttp.client3._
@@ -11,8 +10,8 @@ object Update extends IOApp.Simple {
 
   val run = for
     // Get config parameters
-    _   <- IO.print("Please select day: ")
-    day <- IO.readLine.map(_.toInt)
+    _ <- IO.unit
+    day = java.time.LocalDate.now().getDayOfMonth
     sessionId = System.getenv("SESSION_ID")
     year = System.getenv("YEAR")
     uri = uri"https://adventofcode.com/$year/day/$day/input"
